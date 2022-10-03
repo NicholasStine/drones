9cd03ccef7f449020a6793f0e0193e43d0513e17
Daniel Hoven <dhoven@my.gcu.edu>
2022-10-02 22:18:00 -0700

    Added 3DOF physics

--- drone.py
+++ drone.py
@@ -1,31 +1,70 @@
 # ALRIGHT LET'S DO THIS, leeeeeroooooooyy JEEEEENNKKIIIIIINNNNS!!!
 # WITH THE JAZZIEST PLEASURE, I
 # INTRODUCE, MY MAN, DUKE SILVER!!!
-from physics import Position, Vector
+from physics import Position, Vector, BodyState
 import pygame
+from numpy import array
+
+# move this into your config or whatever
+# initial positions
+
+X = [[0,0,0],[10,0,0]]
+theta = [0,0,0]
+
+# simulation timestep, link this to your pygame step size
+
+dt = 0.01
 
 class Drone():
     def __init__(self, screen):
         # Screen (for drawing)
         self.screen = screen
+
+        """
+
+        y
+        ^
+        |
+        |
+        |
+        . ------ > x
+
+        """
         
         # Physics
-        self.pos = Position(300, 20)
-        self.vel = Vector(0)
-        
-        # Dimensions
+        self.body = BodyState(X, theta) # body state
+        self.pos_x = self.body.X[0][0]  # reference position X
+        self.pos_y = self.Body.X[1][0]  # reference position Y
+        self.mass = 0.3                 # mass
+        self.inertia = 0.25             # moment of inertia
+        self.accels = array([0,0,0])    # initial accelerations (thrust induced)
+        self.leftPropPos = 50          # length from center of mass to left propeller
+        self.rightPropPos = 50       # length from center of mass to right propeller
+                                # Dimensions
         self.strut = (100, 10)
 
+    def Motormixing(leftThrust,rightThrust)
+        # motor mixing algorithm
+
+        self.accels[0] = 0; # no thrust in the x direction, only in y
+        self.accels[1] = (leftThrust + rightThrust) / mass # F = ma
+        self.accels[2] = (leftThrust * self.leftPropPos)/self.inertia 
+                         - (rightThrust * self.rightPropPos)/self.inertia
+
+
     def render(self, gravity):
-        self.pos.applyVectors(self.vel, gravity)
-        self.pos.moveStep()
+        
+        self.Motormixing(.1,.1)
+        self.body.timestep(self.accels,dt)
+        self.pos_x = self.body.X[0][0]
+        self.pos_y = self.body.Y[1][0]
         pygame.draw.rect(self.screen, (90, 2, 33), pygame.Rect(*self.drawStrut()))
 
     def drawStrut(self):
         w, h = self.strut
         half_height = h / 2
         half_width = w / 2
-        return (self.pos.x - half_width, self.pos.y - half_height, w, h)
+        return (self.pos_x - half_width, self.pos_y - half_height, w, h)
 
 class Thruster():
     def __init__(self):
--- physics.py
+++ physics.py
@@ -1,5 +1,6 @@
 # Positions, Vectors & Miscellaneous Mathematics, Oh My!
 from math import sin, cos, pi
+import numpy as np
 
 DIRECTIONS = {
     'UP': 3 * pi / 2,
@@ -24,6 +25,37 @@ class Position():
     def moveStep(self):
         self.x += self.dx
         self.y += self.dy
+
+class BodyState(): # integrate EOM for a 3 DOF rigid body in free space
+
+
+    def __init__(self, X, Theta):
+        # positional state vector
+        self.X = np.array([[0,0,0],[0,0,0]]) # [vector][state]
+        self.X = X
+
+    
+        # angular state vector 
+        self.Theta = np.array([0,0,0])
+        self.Theta = Theta                
+ 
+    def timeStep(self,accels,dt): # calculate accelerations externally
+        
+        self.X[0][2] = accels[0]*cos(self.Theta[0]) - accels[1]*sin(self.Theta[0]) # (2D transform)
+        self.X[0][1] = self.X[0][1] + (self.X[0][2] * dt)   # integrate velocity x
+        self.X[0][0] = self.X[0][0] + (self.X[0][1] * dt)   # integrate position x
+
+        self.X[1][2] = accels[1]*cos(self.Theta[0]) + accels[0]*sin(self.Theta[0])                       
+        self.X[1][1] = self.X[1][1] + (self.X[1][2] * dt)   # integrate velocity y
+        self.X[1][0] = self.X[1][0] + (self.X[1][1] * dt)   # integrate position y
+        
+        
+
+        self.Theta[2] = accels[3]
+        self.Theta[1] = self.Theta[1] + self.Theta[2] * dt
+        self.Theta[0] = self.Theta[0] + self.Theta[1] * dt
+
+        
 
 class Vector():
     def __init__(self, initial_mag=0, initial_theta='DOWN'):
